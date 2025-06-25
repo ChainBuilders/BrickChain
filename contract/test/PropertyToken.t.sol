d898524172df60780d0b09d92cbc3f6c67dbfa8c
olujimiAdebakin <omoladebu231@gmail.com>
2025-06-25 05:43:14 +0100

    implememnted test for KYCManager contract

--- contract/test/PropertyToken.t.sol
+++ contract/test/PropertyToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import "../src/tokens/PropertyToken.sol";
 import "../src/access/AccessManager.sol";
 import "../test/mocks/MockAccessManager.sol";
